ec81bf4689208207e20e0f90f42fd3368198815f
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-24 23:58:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,11 +2,11 @@
 require 'pry'
 
 def generate_star_date
-  binding.pry
    (rand(100000) + 400000) / 10.0
 end
 
 def state_log(star_date)
+  binding.pry
   "Captain's Log, star date #{generate_star_date}."
 end
 
